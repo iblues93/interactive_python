52129e1b1899bad9b26ac2274f79e227e5f7b26e
iBlues93 <oxitachixo@hotmail.com>
2018-06-05 14:13:45 +0800

    Added input receiver

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-test
+# utilities
+import itertools
+
+def main():
+
+    for i, user_input in get_user_input():
+        print(i,user_input)
+
+def get_user_input():
+    
+    for i in itertools.count():
+        yield i, input('In[{}]: '.format(i))
+
+if __name__ == "__main__":
+    main()
