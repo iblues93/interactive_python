5a1b3914644c59c724fb55c9c24c436b400bffc3
iBlues93 <oxitachixo@hotmail.com>
2018-06-05 15:30:35 +0800

    Added variable logging

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ import itertools
 
 def main():
     user_globals = {}
+    save_user_globals(user_globals)
     for i, user_input in get_user_input():
         # process user input
         user_globals = execute_user_input(i,user_input,user_globals)
+        save_user_globals(user_globals)
+
+def save_user_globals(user_globals,path="user_globals.txt"):
+
+    with open(path,"w") as f:
+        f.write("Name    Value    Type\n")
+        for key,val,val_type in only_user_variables(user_globals):
+            f.write("{} : {} ({})\n".format(key,val,val_type))
+
+def only_user_variables(user_globals):
+
+    for key,val in user_globals.items():
+        # Remove dunder items which are built-in variables
+        # Remove Imports
+        if (not key.startswith("__") or not key.endswith("__")) and val.__class__.__name__ != "module":
+            yield (key,val,val.__class__.__name__)
 
 def exec_function(user_input):
     # higher level wrapper to determine if user input is statement or expression
@@ -22,6 +39,7 @@ def exec_function(user_input):
 
 def execute_user_input(i,user_input,user_globals):
     # get the function to be executed with the parameters user_input and user_globals
+    # Since user_globals is going to get updated, copy it as a local variable, then return it. [Referential Transparency]
     user_globals = user_globals.copy()
     try:
         res = exec_function(user_input)(user_input,user_globals)
