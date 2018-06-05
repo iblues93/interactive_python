839ba976d3d5e97c5faeb4933dc8bbad277f29da
iBlues93 <oxitachixo@hotmail.com>
2018-06-05 14:36:36 +0800

    Added user input compilation to determine exec or eval

--- main.py
+++ main.py
@@ -2,14 +2,37 @@
 import itertools
 
 def main():
-
+    user_globals = {}
     for i, user_input in get_user_input():
-        print(i,user_input)
+        # process user input
+        execute_user_input(i,user_input,user_globals)
+
+def exec_function(user_input):
+    # higher level wrapper to determine if user input is statement or expression
+    # returns the executable function
+    # Try to compile the user input
+    # if executable use exec()
+    # if expression use eval()
+
+    try:
+        compile(user_input,'<stdin>','eval')
+    except SyntaxError:
+        return exec
+    return eval
+
+def execute_user_input(i,user_input,user_globals):
+    # get the function to be executed with the parameters user_input and user_globals
+    exec_function(user_input)(user_input,user_globals)
 
 def get_user_input():
-    
-    for i in itertools.count():
-        yield i, input('In[{}]: '.format(i))
+     for i in itertools.count():
+        try:
+            yield i, input('In[{}]: '.format(i))
+        # except KeyboardInterrupt:
+        #     pass
+        except EOFError:
+            break
+
 
 if __name__ == "__main__":
     main()
